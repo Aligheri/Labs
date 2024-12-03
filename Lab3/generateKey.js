6d8ad1aa143ab1ad12c491885b57b6e0239cea01
Aligheri <ivanyevsieiev@gmail.com>
2024-12-03 15:48:08 +0200

    Added Lab3: functions random, generateKey, ipToNumber и introspection

---
+++ Lab3/generateKey.js
@@ -0,0 +1,20 @@
+function random(min, max) {
+    if (max === undefined) {
+        max = min;
+        min = 0;
+    }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateKey(length, characters) {
+    let key = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = random(0, characters.length - 1);
+        key += characters[randomIndex];
+    }
+    return key;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key); // Пример: 'eg599gb60q926j8i'
