b2f5a429def5b3d3854f082a5259fc626f5bfbc1
Aligheri <ivanyevsieiev@gmail.com>
2024-12-03 20:45:38 +0200

    Rollback changes for seq.js

--- Lab5/seq.js
+++ Lab5/seq.js
@@ -15,17 +15,17 @@ function seq(...args) {
 
 console.log(
     seq(x => x + 7)
-    (x => x * 4)(5)
+    (x => x * 2)(5)
 );
 
 console.log(
     seq(x => x * 2)
-    (x => x + 2)(5)
+    (x => x + 7)(5)
 );
 
 console.log(
     seq(x => x + 1)
-    (x => x * 8)
+    (x => x * 2)
     (x => x / 3)
     (x => x - 4)(7)
 );
